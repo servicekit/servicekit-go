80e356547b82370bef009e9149c3d36f8faef79e
sosowang <wowsososo@gmail.com>
2017-07-09 12:55:09 +0800

    added shortcut func for grpc

--- grpchelper/conn.go
+++ grpchelper/conn.go
@@ -4,9 +4,22 @@ import (
     "google.golang.org/grpc"
     "google.golang.org/grpc/credentials"
     "google.golang.org/grpc/naming"
+
+    balancer "github.com/servicekit/servicekit-go/balancer/consul"
+    "github.com/servicekit/servicekit-go/coordinator"
 )
 
-func BalanceDial(creds credentials.TransportCredentials, resolver naming.Resolver) (*grpc.ClientConn, error) {
+func BalanceDial(credPath string, c coordinator.Coordinator, service string, tag string, log *logger.Logger) (*grpc.ClientConn, error) {
+    creds, err := credentials.NewClientTLSFromFile(credPath, service)
+    if err != nil {
+        return nil, err
+    }
+
+    resolver, err := balancer.GetResolver(c, service, tag, log)
+    if err != nil {
+        return nil, err
+    }
+
     conn, err := grpc.Dial(
         "",
         grpc.WithTransportCredentials(creds),
