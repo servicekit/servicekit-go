3e268cf886eee0ecf3a252ed685df13f079fc492
sosowang <wowsososo@gmail.com>
2017-07-11 16:26:43 +0800

    update grpc

---
+++ service/grpc.go
@@ -0,0 +1,70 @@
+package service
+
+import (
+    "time"
+)
+
+type GRPCServer interface {
+    Serve(ctx context.Context, network, addr, pem, key string) error
+}
+
+type GRPCService struct {
+    ID      string
+    Service string
+    Tags    []string
+    Address string
+    Port    int
+
+    server GRPCServer
+
+    TTL time.Duration
+
+    c coordinator.Coordinator
+
+    log *logger.Logger
+
+    errorChan chan error
+}
+
+func NewGRPCService(id string, service string, tags []string, address string, port int, server GRPCServer, ttl time.Duration, c coordinator.Coordinator, log *logger.Logger) *GRPCService {
+    return &GRPCService{
+        ID:      id,
+        Service: service,
+        Tags:    tags,
+        Address: address,
+        Port:    port,
+
+        server: server,
+
+        TTL: ttl,
+        c:   c,
+        log: log,
+
+        errorChan: make(chan error),
+    }
+}
+
+func (g *GRPCService) Start(ctx context.Context, delayRegisterTime time.Duration) error {
+    go func() {
+        err := g.server.Serve(
+            ctx,
+            "tcp",
+            fmt.Sprintf("%s:%d", g.Address, g.Port),
+            g.pem,
+            g.key,
+        )
+        g.errorChan <- err
+    }()
+
+    time.Sleep(delayRegisterTime)
+
+    var err error
+
+    select {
+    case err = <-g.errorChan:
+    default:
+        err = c.Register(ctx, s)
+    }
+
+    return err
+}
