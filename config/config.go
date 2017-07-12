edfb3f751d766e171cde31d14678a9d4386b970c
sosowang <wowsososo@gmail.com>
2017-07-12 10:20:08 +0800

    added config field: serviceid

--- config/config.go
+++ config/config.go
@@ -1,46 +1,47 @@
 package config
 
 import (
-    "fmt"
-    "log/syslog"
+	"fmt"
+	"log/syslog"
 
-    "github.com/kelseyhightower/envconfig"
+	"github.com/kelseyhightower/envconfig"
 )
 
 type ServiceENV string
 type ServiceVersion string
 
 func NewCustomConfigPrefix(s string) string {
-    return fmt.Sprintf("%s_%s", "SERVICE_CUSTOM", s)
+	return fmt.Sprintf("%s_%s", "SERVICE_CUSTOM", s)
 }
 
 func newConfigPrefix(s string) string {
-    return fmt.Sprintf("%s_%s", "SERVICE", s)
+	return fmt.Sprintf("%s_%s", "SERVICE", s)
 }
 
 const (
-    ServiceENVDev     ServiceENV = "dev"
-    ServiceENVStaging ServiceENV = "staging"
-    ServiceENVProd    ServiceENV = "production"
+	ServiceENVDev     ServiceENV = "dev"
+	ServiceENVStaging ServiceENV = "staging"
+	ServiceENVProd    ServiceENV = "production"
 )
 
 type ServiceConfig struct {
-    ServiceName    string
-    ServiceENV     ServiceENV
-    ServiceVersion string
-    ServiceTags    []string
+	ServiceID      string
+	ServiceName    string
+	ServiceENV     ServiceENV
+	ServiceVersion string
+	ServiceTags    []string
 
-    TraceHost string
-    TracePort int
+	TraceHost string
+	TracePort int
 
-    LoggerNetwork  string
-    LoggerADDR     string
-    LoggerPriority syslog.Priority
+	LoggerNetwork  string
+	LoggerADDR     string
+	LoggerPriority syslog.Priority
 }
 
 func NewServiceConfig() *ServiceConfig {
-    var serviceConfig ServiceConfig
-    envconfig.Process(newConfigPrefix("SERVICE"), &serviceConfig)
-    fmt.Println(serviceConfig)
-    return &serviceConfig
+	var serviceConfig ServiceConfig
+	envconfig.Process(newConfigPrefix("SERVICE"), &serviceConfig)
+	fmt.Println(serviceConfig)
+	return &serviceConfig
 }
