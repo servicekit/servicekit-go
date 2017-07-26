9bbf3c811b07004952cdcabeab20364643ead031
sosowang <wowsososo@gmail.com>
2017-07-26 22:18:37 +0800

    update

--- grpchelper/rid.go
+++ grpchelper/rid.go
@@ -1,80 +1,80 @@
 package grpchelper
 
 import (
-    "fmt"
+	"fmt"
 
-    "github.com/rs/xid"
-    "golang.org/x/net/context"
-    "google.golang.org/grpc/metadata"
+	"github.com/rs/xid"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
 )
 
 // RequestIDKey is metadata key name for request ID
 var RequestIDKey = "request-id"
 
 func HandleRequestID(ctx context.Context) string {
-    md, ok := metadata.FromContext(ctx)
-    if !ok {
-        return newRequestID()
-    }
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		return newRequestID()
+	}
 
-    header, ok := md[RequestIDKey]
-    if !ok || len(header) == 0 {
-        return newRequestID()
-    }
+	header, ok := md[RequestIDKey]
+	if !ok || len(header) == 0 {
+		return newRequestID()
+	}
 
-    requestID := header[0]
-    if requestID == "" {
-        return newRequestID()
-    }
+	requestID := header[0]
+	if requestID == "" {
+		return newRequestID()
+	}
 
-    return requestID
+	return requestID
 }
 
 func HandleRequestIDChain(ctx context.Context) string {
-    md, ok := metadata.FromContext(ctx)
-    if !ok {
-        return newRequestID()
-    }
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		return newRequestID()
+	}
 
-    header, ok := md[RequestIDKey]
-    if !ok || len(header) == 0 {
-        return newRequestID()
-    }
+	header, ok := md[RequestIDKey]
+	if !ok || len(header) == 0 {
+		return newRequestID()
+	}
 
-    requestID := header[0]
-    if requestID == "" {
-        return newRequestID()
-    }
+	requestID := header[0]
+	if requestID == "" {
+		return newRequestID()
+	}
 
-    return fmt.Sprintf("%s,%s", requestID, newRequestID())
+	return fmt.Sprintf("%s,%s", requestID, newRequestID())
 }
 
 func newRequestID() string {
-    return xid.New().String()
+	return xid.New().String()
 }
 
 func UpdateContextWithRequestID(ctx context.Context, requestID string) context.Context {
-    md := metadata.New(map[string]string{RequestIDKey: requestID})
-    _md, ok := metadata.FromContext(ctx)
-    if ok {
-        md = metadata.Join(_md, md)
-    }
+	md := metadata.New(map[string]string{RequestIDKey: requestID})
+	_md, ok := metadata.FromContext(ctx)
+	if ok {
+		md = metadata.Join(_md, md)
+	}
 
-    ctx = metadata.NewContext(ctx, md)
+	ctx = metadata.NewContext(ctx, md)
 
-    return ctx
+	return ctx
 }
 
 func GetRequestID(ctx context.Context) string {
-    md, ok := metadata.FromContext(ctx)
-    if ok == false {
-        return ""
-    }
+	md, ok := metadata.FromContext(ctx)
+	if ok == false {
+		return ""
+	}
 
-    header, ok := md[RequestIDKey]
-    if !ok || len(header) == 0 {
-        return ""
-    }
+	header, ok := md[RequestIDKey]
+	if !ok || len(header) == 0 {
+		return ""
+	}
 
-    return header[0]
+	return header[0]
 }
--- logger/logger.go
+++ logger/logger.go
@@ -1,276 +1,280 @@
 package logger
 
 import (
-    "bytes"
-    "fmt"
-    "log/syslog"
-    "runtime"
-    "strings"
+	"bytes"
+	"fmt"
+	"log/syslog"
+	"runtime"
+	"strings"
 
-    log "github.com/sirupsen/logrus"
-    logrus_syslog "github.com/sirupsen/logrus/hooks/syslog"
+	log "github.com/sirupsen/logrus"
+	logrus_syslog "github.com/sirupsen/logrus/hooks/syslog"
 
-    "github.com/servicekit/servicekit-go/config"
-    "github.com/servicekit/servicekit-go/grpchelper"
+	"github.com/servicekit/servicekit-go/config"
+	"github.com/servicekit/servicekit-go/grpchelper"
+)
+
+const (
+	RequestIDKey = "request-id"
 )
 
 func insert(slice []interface{}, insertion interface{}) []interface{} {
-    result := make([]interface{}, len(slice)+1)
-    result[0] = insertion
-    copy(result[1:], slice)
-    return result
+	result := make([]interface{}, len(slice)+1)
+	result[0] = insertion
+	copy(result[1:], slice)
+	return result
 }
 
 type LogFormatter struct {
 }
 
 func (f *LogFormatter) Format(entry *log.Entry) ([]byte, error) {
-    var b *bytes.Buffer
-    if entry.Buffer != nil {
-        b = entry.Buffer
-    } else {
-        b = &bytes.Buffer{}
-    }
+	var b *bytes.Buffer
+	if entry.Buffer != nil {
+		b = entry.Buffer
+	} else {
+		b = &bytes.Buffer{}
+	}
 
-    b.WriteString(strings.ToUpper(entry.Level.String()))
-    b.WriteString(" ")
-    b.WriteString(entry.Message)
-    b.WriteString("\n")
-    return b.Bytes(), nil
+	b.WriteString(strings.ToUpper(entry.Level.String()))
+	b.WriteString(" ")
+	b.WriteString(entry.Message)
+	b.WriteString("\n")
+	return b.Bytes(), nil
 }
 
 type Logger struct {
-    logger *log.Logger
+	logger *log.Logger
 }
 
 func NewLogger(serviceName, serviceVersion string, serviceENV config.ServiceENV, network, addr string, priority syslog.Priority) (*Logger, error) {
-    logger := &Logger{}
-    logger.logger = log.New()
+	logger := &Logger{}
+	logger.logger = log.New()
 
-    logger.logger.Formatter = &LogFormatter{}
+	logger.logger.Formatter = &LogFormatter{}
 
-    hook, err := logrus_syslog.NewSyslogHook(network, addr, priority, fmt.Sprintf("%s_%s_%s", serviceName, serviceVersion, serviceENV))
-    if err != nil {
-        return nil, err
-    } else {
-        logger.logger.Hooks.Add(hook)
-        return logger, nil
-    }
+	hook, err := logrus_syslog.NewSyslogHook(network, addr, priority, fmt.Sprintf("%s_%s_%s", serviceName, serviceVersion, serviceENV))
+	if err != nil {
+		return nil, err
+	} else {
+		logger.logger.Hooks.Add(hook)
+		return logger, nil
+	}
 }
 
 func (logger *Logger) Debugf(format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s", file, line, format)
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s", file, line, format)
+	}
 
-    logger.logger.Debugf(f, args...)
+	logger.logger.Debugf(f, args...)
 }
 
 func (logger *Logger) Infof(format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s", file, line, format)
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s", file, line, format)
+	}
 
-    logger.logger.Infof(f, args...)
+	logger.logger.Infof(f, args...)
 }
 
 func (logger *Logger) Printf(format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s", file, line, format)
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s", file, line, format)
+	}
 
-    logger.logger.Printf(f, args...)
+	logger.logger.Printf(f, args...)
 }
 
 func (logger *Logger) Warnf(format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s", file, line, format)
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s", file, line, format)
+	}
 
-    logger.logger.Warnf(f, args...)
+	logger.logger.Warnf(f, args...)
 }
 
 func (logger *Logger) Errorf(format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s", file, line, format)
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s", file, line, format)
+	}
 
-    logger.logger.Errorf(f, args...)
+	logger.logger.Errorf(f, args...)
 }
 
 func (logger *Logger) Fatalf(format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s", file, line, format)
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s", file, line, format)
+	}
 
-    logger.logger.Fatalf(f, args...)
+	logger.logger.Fatalf(f, args...)
 }
 
 func (logger *Logger) Panicf(format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s", file, line, format)
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s", file, line, format)
+	}
 
-    logger.logger.Panicf(f, args...)
+	logger.logger.Panicf(f, args...)
 }
 
 func (logger *Logger) DebugfWithReqID(ctx context.Context, format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(grpchelper.RequestIDKey))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(RequestIDKey))
+	}
 
-    logger.logger.Debugf(f, args...)
+	logger.logger.Debugf(f, args...)
 }
 
 func (logger *Logger) InfofWithReqID(ctx context.Context, format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(grpchelper.RequestIDKey))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(RequestIDKey))
+	}
 
-    logger.logger.Infof(f, args...)
+	logger.logger.Infof(f, args...)
 }
 
 func (logger *Logger) PrintfWithReqID(ctx context.Context, format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(grpchelper.RequestIDKey))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(RequestIDKey))
+	}
 
-    logger.logger.Printf(f, args...)
+	logger.logger.Printf(f, args...)
 }
 
 func (logger *Logger) WarnfWithReqID(ctx context.Context, format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(grpchelper.RequestIDKey))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(RequestIDKey))
+	}
 
-    logger.logger.Warnf(f, args...)
+	logger.logger.Warnf(f, args...)
 }
 
 func (logger *Logger) ErrorfWithReqID(ctx context.Context, format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(grpchelper.RequestIDKey))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(RequestIDKey))
+	}
 
-    logger.logger.Errorf(f, args...)
+	logger.logger.Errorf(f, args...)
 }
 
 func (logger *Logger) FatalfWithReqID(ctx context.Context, format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(grpchelper.RequestIDKey))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(RequestIDKey))
+	}
 
-    logger.logger.Fatalf(f, args...)
+	logger.logger.Fatalf(f, args...)
 }
 
 func (logger *Logger) PanicfWithReqID(ctx context.Context, format string, args ...interface{}) {
-    f := format
+	f := format
 
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(grpchelper.RequestIDKey))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		f = fmt.Sprintf("%s:%d %s reqid: %v", file, line, format, ctx.Value(RequestIDKey))
+	}
 
-    logger.logger.Panicf(f, args...)
+	logger.logger.Panicf(f, args...)
 }
 
 func (logger *Logger) Debug(args ...interface{}) {
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        args = insert(args, fmt.Sprintf("%s:%d ", file, line))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		args = insert(args, fmt.Sprintf("%s:%d ", file, line))
+	}
 
-    logger.logger.Debug(args...)
+	logger.logger.Debug(args...)
 }
 
 func (logger *Logger) Info(args ...interface{}) {
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        args = insert(args, fmt.Sprintf("%s:%d ", file, line))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		args = insert(args, fmt.Sprintf("%s:%d ", file, line))
+	}
 
-    logger.logger.Info(args...)
+	logger.logger.Info(args...)
 }
 
 func (logger *Logger) Print(args ...interface{}) {
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        args = insert(args, fmt.Sprintf("%s:%d ", file, line))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		args = insert(args, fmt.Sprintf("%s:%d ", file, line))
+	}
 
-    logger.logger.Print(args...)
+	logger.logger.Print(args...)
 }
 
 func (logger *Logger) Warn(args ...interface{}) {
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        args = insert(args, fmt.Sprintf("%s:%d ", file, line))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		args = insert(args, fmt.Sprintf("%s:%d ", file, line))
+	}
 
-    logger.logger.Warn(args...)
+	logger.logger.Warn(args...)
 }
 
 func (logger *Logger) Error(args ...interface{}) {
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        args = insert(args, fmt.Sprintf("%s:%d ", file, line))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		args = insert(args, fmt.Sprintf("%s:%d ", file, line))
+	}
 
-    logger.logger.Error(args...)
+	logger.logger.Error(args...)
 }
 
 func (logger *Logger) Fatal(args ...interface{}) {
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        args = insert(args, fmt.Sprintf("%s:%d ", file, line))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		args = insert(args, fmt.Sprintf("%s:%d ", file, line))
+	}
 
-    logger.logger.Fatal(args...)
+	logger.logger.Fatal(args...)
 }
 
 func (logger *Logger) Panic(args ...interface{}) {
-    _, file, line, ok := runtime.Caller(1)
-    if ok {
-        args = insert(args, fmt.Sprintf("%s:%d ", file, line))
-    }
+	_, file, line, ok := runtime.Caller(1)
+	if ok {
+		args = insert(args, fmt.Sprintf("%s:%d ", file, line))
+	}
 
-    logger.logger.Panic(args...)
+	logger.logger.Panic(args...)
 }
